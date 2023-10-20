781f79c2fa395686e62673ad34f1089fb959b953
anferneealviar99 <anferneealviar99@hotmail.com>
2023-10-20 18:41:41 +0800

    script change

--- project2_script.sh
+++ project2_script.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=16
 #SBATCH --ntasks-per-node=4
 #SBATCH --nodes=4
-#SBATCH --time=00:30:00
+#SBATCH --time=00:01:00
 #SBATCH --exclusive
 #SBATCH --mem-per-cpu=32G
 #module load openmpi/4.0.5
