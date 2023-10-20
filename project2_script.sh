d1b9fe714f7b1eaa7485ce7d4ddedca5b554439a
anferneealviar99 <anferneealviar99@hotmail.com>
2023-10-20 14:20:44 +0800

    Added script for the fish school sim

--- project2_script.sh
+++ project2_script.sh
@@ -5,10 +5,11 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --nodes=4
 #SBATCH --time=00:30:00
+#SBATCH --cpus-per-task=4
 #SBATCH --exclusive
 #SBATCH --mem-per-cpu=32G
 #module load openmpi/4.0.5
 
 export OMP_NUM_THREADS=16
-mpicc -fopenmp -o mpi_expr mpi_experiment.c
-srun ./mpi_expr
+mpicc -fopenmp -o fish_school_parallel fish_school_parallel.c
+srun ./fish_school_parallel
